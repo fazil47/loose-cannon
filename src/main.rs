9b9514f8066ed1595abc986bb12c820949025a5d
Fazil Babu <fazil47babu@gmail.com>
2022-11-06 11:27:25 +0530

    Taking in mouse input

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() {
         .add_plugin(EditorPlugin)
         .add_startup_system(setup)
         .add_system(gravity)
+        .add_system(player_input)
         .run();
 }
 
@@ -33,7 +34,10 @@ fn setup(
             coefficient: 2.0,
             combine_rule: CoefficientCombineRule::Max,
         })
-        .insert(Restitution::coefficient(0.0));
+        .insert(Restitution {
+            coefficient: 0.0,
+            combine_rule: CoefficientCombineRule::Max,
+        });
 
     // player
     commands
@@ -43,18 +47,25 @@ fn setup(
                 subdivisions: 10,
             })),
             material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            transform: Transform::from_xyz(0.0, 0.0, 22.5),
+            transform: Transform::from_xyz(0.0, 0.0, 21.5),
             ..default()
         })
         .insert(Collider::ball(1.0))
         .insert(RigidBody::Dynamic)
-        .insert(ColliderMassProperties::Density(2.0))
+        .insert(Damping {
+            linear_damping: 0.1,
+            angular_damping: 0.2,
+        })
+        .insert(ColliderMassProperties::Density(1.0))
         .insert(GravityScale(0.0))
         .insert(Friction {
             coefficient: 2.0,
             combine_rule: CoefficientCombineRule::Max,
         })
-        .insert(Restitution::coefficient(0.0))
+        .insert(Restitution {
+            coefficient: 0.0,
+            combine_rule: CoefficientCombineRule::Max,
+        })
         .insert(ExternalForce {
             force: Vec3::new(0.0, 0.0, 0.0),
             torque: Vec3::new(0.0, 0.0, 0.0),
@@ -102,9 +113,46 @@ fn setup(
     });
 }
 
+fn player_input(
+    buttons: Res<Input<MouseButton>>,
+    windows: Res<Windows>,
+    mut player_query: Query<(&Transform, &mut ExternalImpulse)>,
+    camera_query: Query<&Transform, With<Camera3d>>,
+) {
+    let window = windows.get_primary().unwrap();
+    let camera = camera_query.iter().next().unwrap();
+
+    // If cursor is inside the window
+    if let Some(_position) = window.cursor_position() {
+        if buttons.just_pressed(MouseButton::Left) {
+            for (transform, mut impulse) in player_query.iter_mut() {
+                println!(
+                    "Cursor position in screen space {}, in world space {}, player translation {}, rotation {}",
+                    _position, window_to_world(_position, window, camera), transform.translation, transform.rotation
+                );
+                impulse.impulse = Vec3::new(128.0, 0.0, 0.0);
+            }
+        }
+    }
+}
+
 // Custom gravity which acts towards the center of the planet (which is at the origin)
 fn gravity(mut query: Query<(&Transform, &mut ExternalForce)>) {
     for (transform, mut force) in query.iter_mut() {
-        force.force = transform.translation.normalize_or_zero() * -9.81 * 10.0;
+        force.force = transform.translation.normalize_or_zero() * -9.81 * 100.0;
     }
+}
+
+// From https://stackoverflow.com/a/65633668/7658270, licensed under CC BY-SA 4.0
+// Transform position from screen space to world space
+fn window_to_world(position: Vec2, window: &Window, camera: &Transform) -> Vec3 {
+    // Center in screen space
+    let norm = Vec3::new(
+        position.x - window.width() / 2.0,
+        position.y - window.height() / 2.0,
+        0.0,
+    );
+
+    // Apply camera transform
+    camera.mul_vec3(norm)
 }
