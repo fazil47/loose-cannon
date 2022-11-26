26474809cc3c42beae374cd2b8193004dd4bcda0
Fazil Babu <fazil47babu@gmail.com>
2022-11-26 15:34:05 +0530

    Split up player_input system into smaller systems

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use bevy_rapier3d::prelude::*;
 
 use loose_cannon::cubemap::{construct_skybox, Cubemap, CubemapMaterial};
 
-// TODO: Set player cursor position in a resource
 // TODO: Refactor
 // TODO: Restrict player collider altitude
 // TODO: game over when a cannon ball hits the player
@@ -48,6 +47,24 @@ struct CannonBall {}
 #[derive(Resource)]
 struct ShootTimer(Timer);
 
+#[derive(Resource)]
+struct PlayerInput {
+    last_valid_cursor_pos: Option<Vec2>,
+}
+
+#[derive(Resource)]
+struct PlayerMeshDesiredTransform {
+    position: Vec3,
+    tangent: Vec3,
+    local_up: Vec3,
+    local_forward: Vec3,
+}
+
+struct ShootEvent {
+    position: Vec3,
+    direction: Vec3,
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -58,10 +75,14 @@ fn main() {
         .add_plugin(DebugLinesPlugin::with_depth_test(true))
         .add_startup_system(setup)
         .add_system(gravity)
-        .add_system(player_input)
+        .add_system(handle_player_input)
+        .add_system(set_player_mesh_transform)
+        .add_system(apply_player_collider_impulse)
+        .add_system(shoot_cannon_ball)
         .add_system(move_camera)
         .add_system(handle_collisions)
         .add_system(construct_skybox)
+        .add_event::<ShootEvent>()
         .run();
 }
 
@@ -71,12 +92,17 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
+    // Insert resource to keep track of player cursor position
+    commands.insert_resource(PlayerInput {
+        last_valid_cursor_pos: Option::None,
+    });
+
     // Insert resouce to keep track of time until the next cannon ball can be fired
     let mut timer = Timer::from_seconds(FIRE_DELAY, TimerMode::Once);
     timer.tick(timer.duration());
     commands.insert_resource(ShootTimer(timer));
 
-    // planet
+    // Planet
     commands
         .spawn(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Icosphere {
@@ -102,7 +128,7 @@ fn setup(
             combine_rule: CoefficientCombineRule::Max,
         });
 
-    // player mesh
+    // Player mesh
     commands
         .spawn(SceneBundle {
             scene: asset_server.load("models/cannon.glb#Scene0"),
@@ -112,7 +138,15 @@ fn setup(
         .insert(Name::new("PlayerMesh"))
         .insert(PlayerMesh {});
 
-    // player collider
+    // Resource to store desired transform of player mesh
+    commands.insert_resource(PlayerMeshDesiredTransform {
+        position: Vec3::new(0.0, 0.0, PLANET_SIZE + PLAYER_SIZE),
+        tangent: Vec3::new(0.0, 1.0, 0.0),
+        local_up: Vec3::new(0.0, 0.0, 1.0),
+        local_forward: Vec3::new(0.0, 1.0, 0.0),
+    });
+
+    // Player collider
     commands
         .spawn(TransformBundle::from(Transform::from_xyz(
             0.0,
@@ -146,7 +180,7 @@ fn setup(
             torque_impulse: Vec3::new(0.0, 0.0, 0.0),
         });
 
-    // directional light - sun
+    // Directional light - sun
     commands
         .spawn(DirectionalLightBundle {
             directional_light: DirectionalLight {
@@ -178,7 +212,7 @@ fn setup(
         })
         .insert(Name::new("Sun"));
 
-    // directional light - moon
+    // Directional light - moon
     commands
         .spawn(DirectionalLightBundle {
             directional_light: DirectionalLight {
@@ -210,20 +244,20 @@ fn setup(
         })
         .insert(Name::new("Moon"));
 
-    // camera
+    // Camera
     // Querying doesn't work if I name the camera entity
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(0.0, 0.0, CAMERA_DISTANCE).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
-    // ambient light
+    // Ambient light
     commands.insert_resource(AmbientLight {
         color: Color::rgb(1.0, 1.0, 0.8),
         brightness: 0.4,
     });
 
-    // skybox
+    // Skybox
     let skybox_handle = asset_server.load(CUBEMAP.0);
     commands.insert_resource(Cubemap {
         image_handle: skybox_handle,
@@ -231,41 +265,32 @@ fn setup(
     });
 }
 
-// Handle player input
-fn player_input(
-    commands: Commands,
-    meshes: ResMut<Assets<Mesh>>,
-    materials: ResMut<Assets<StandardMaterial>>,
-    buttons: Res<Input<MouseButton>>,
-    windows: Res<Windows>,
+// Handles change in cursor position, updates PlayerMeshDesiredTransform resource
+// And sends ShootEvent on LMB click based on the ShootTimer resource
+fn handle_player_input(
+    mut player_input: ResMut<PlayerInput>,
+    mut player_mesh_desired_transform: ResMut<PlayerMeshDesiredTransform>,
+    mut shoot_timer: ResMut<ShootTimer>,
+    time: Res<Time>,
     rapier_context: Res<RapierContext>,
     mut lines: ResMut<DebugLines>,
-    time: Res<Time>,
-    mut shoot_timer: ResMut<ShootTimer>,
-    mut player_collider_query: Query<
-        (&Transform, &mut ExternalImpulse),
-        (With<PlayerCollider>, Without<PlayerMesh>),
-    >,
-    mut player_mesh_query: Query<&mut Transform, (With<PlayerMesh>, Without<PlayerCollider>)>,
-    camera_query: Query<
-        (&GlobalTransform, &Camera),
-        (Without<ExternalImpulse>, Without<PlayerMesh>),
-    >,
+    buttons: Res<Input<MouseButton>>,
+    windows: Res<Windows>,
+    mut ev_shoot: EventWriter<ShootEvent>,
+    camera_query: Query<(&GlobalTransform, &Camera)>,
+    player_collider_query: Query<&Transform, With<PlayerCollider>>,
 ) {
     let window: &Window = windows.get_primary().unwrap();
     let (camera_transform, camera) = camera_query.iter().next().unwrap();
-    let (player_c_transform, mut player_c_impulse) = player_collider_query.single_mut();
-    let mut player_mesh_transform = player_mesh_query.single_mut();
+    let player_collider_transform = player_collider_query.single();
 
-    // Player translation should be the same as the player collider translation
-    player_mesh_transform.translation = player_c_transform.translation.normalize() * PLANET_SIZE;
-
-    // Rotate player transform such that it's up vector is in the same direction as player's translation vector
-    player_mesh_transform.set_down(Vec3::ZERO, camera_transform.up());
+    player_mesh_desired_transform.position = player_collider_transform.translation;
+    player_mesh_desired_transform.local_up = camera_transform.back();
+    player_mesh_desired_transform.local_forward = camera_transform.up();
 
     // If cursor is inside the window
     if let Some(cursor_pos) = window.cursor_position() {
-        // Make a raycast from cursor world position parallet to camera direction
+        // Make a raycast from cursor world position parallel to camera direction
         let ray = camera
             .viewport_to_world(camera_transform, cursor_pos)
             .unwrap();
@@ -277,32 +302,15 @@ fn player_input(
         if let Some((_entity, toi)) =
             rapier_context.cast_ray(ray.origin, ray.direction, max_toi, solid, filter)
         {
+            // Set player_input resource
+            player_input.last_valid_cursor_pos = Option::Some(cursor_pos);
+
             // Get the point on the planet where the raycast hit
             let hit_point = ray.origin + (ray.direction * toi);
 
-            // Get the unit vector in the direction of the vector from the hit point to the player
-            let hit_to_player = (hit_point - player_c_transform.translation).normalize();
+            let tangent = get_tangent_helper(hit_point, player_collider_transform);
 
-            // Cross hit_to_player and player collider's normalized translation vector to get the tangent perpendicular to the desired direction
-            let tangent =
-                (hit_to_player.cross(player_c_transform.translation.normalize())).normalize();
-
-            // Cross again to get the desired direction
-            let tangent = tangent.cross(player_c_transform.translation.normalize());
-
-            if SHOW_DEBUG_LINES {
-                lines.line_colored(
-                    player_c_transform.translation,
-                    player_c_transform.translation + tangent,
-                    1.0,
-                    Color::GREEN,
-                );
-            }
-
-            let target = player_mesh_transform.translation + tangent;
-
-            // player_transform.rotate_local_y(player_mesh_angle);
-            player_mesh_transform.look_at(target, camera_transform.back());
+            player_mesh_desired_transform.tangent = tangent;
 
             // the player can shoot only after the timer is up
             if !shoot_timer.0.tick(time.delta()).finished() {
@@ -317,16 +325,152 @@ fn player_input(
 
                 shoot_timer.0.reset();
 
-                player_c_impulse.impulse = tangent * PLAYER_IMPULSE_MAGNITUDE;
+                ev_shoot.send(ShootEvent {
+                    position: player_collider_transform.translation
+                        - (tangent * CANNON_BALL_INITIAL_OFFSET),
+                    direction: -tangent,
+                });
+            }
+        } else if let Some(cursor_pos) = player_input.last_valid_cursor_pos {
+            let ray = camera
+                .viewport_to_world(camera_transform, cursor_pos)
+                .unwrap();
 
-                // shoot cannon ball
-                shoot_cannon_ball(
-                    commands,
-                    meshes,
-                    materials,
-                    player_mesh_transform.translation - (tangent * CANNON_BALL_INITIAL_OFFSET),
-                    -tangent,
-                )
+            // This still needs to be checked because last_valid_cursor_pos won't be valid
+            // if the game window's position or size has changed
+            if let Some((_entity, toi)) =
+                rapier_context.cast_ray(ray.origin, ray.direction, max_toi, solid, filter)
+            {
+                let hit_point = ray.origin + (ray.direction * toi);
+
+                let tangent = get_tangent_helper(hit_point, player_collider_transform);
+
+                player_mesh_desired_transform.tangent = tangent;
+            }
+        }
+    }
+}
+
+// Calculates the tangent in the direction of the vector from the player collider to the hit point on the planet
+fn get_tangent_helper(hit_point: Vec3, player_collider_transform: &Transform) -> Vec3 {
+    // Get the unit vector in the direction of the vector from the hit point to the player
+    let hit_to_player_collider = (hit_point - player_collider_transform.translation).normalize();
+
+    // Cross hit_to_player and player collider's normalized translation vector to get the tangent perpendicular to the desired direction
+    let tangent = (hit_to_player_collider.cross(player_collider_transform.translation.normalize()))
+        .normalize();
+
+    // Cross again to get the desired direction
+    let tangent = tangent.cross(player_collider_transform.translation.normalize());
+
+    tangent
+}
+
+// Sets the player mesh's transform based on value of PlayerMeshDesiredTransform resource
+fn set_player_mesh_transform(
+    mut player_mesh_query: Query<&mut Transform, With<PlayerMesh>>,
+    player_mesh_desired_transform: Res<PlayerMeshDesiredTransform>,
+) {
+    let mut player_mesh_transform = player_mesh_query.single_mut();
+
+    // Player translation should be the same as the player collider translation
+    player_mesh_transform.translation =
+        player_mesh_desired_transform.position.normalize() * PLANET_SIZE;
+
+    // Rotate player transform such that it's up vector is in the same direction as player's translation vector
+    player_mesh_transform.set_down(Vec3::ZERO, player_mesh_desired_transform.local_up);
+
+    let target = player_mesh_transform.translation + player_mesh_desired_transform.tangent;
+
+    // Rotate player transform such that it's looking at the target
+    player_mesh_transform.look_at(target, player_mesh_desired_transform.local_up);
+}
+
+// Applies an impulse to play collider when a ShootEvent is triggered
+fn apply_player_collider_impulse(
+    mut player_collider_query: Query<&mut ExternalImpulse, With<PlayerCollider>>,
+    mut ev_shoot: EventReader<ShootEvent>,
+) {
+    let mut player_collider_impulse = player_collider_query.single_mut();
+
+    for ev in ev_shoot.iter() {
+        player_collider_impulse.impulse = -ev.direction * PLAYER_IMPULSE_MAGNITUDE;
+    }
+}
+
+// Spawns and shoots a cannon ball when a ShootEvent is triggered
+fn shoot_cannon_ball(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut ev_shoot: EventReader<ShootEvent>,
+) {
+    for ev in ev_shoot.iter() {
+        commands
+            .spawn(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Icosphere {
+                    radius: PLAYER_SIZE / 2.0,
+                    subdivisions: 16,
+                })),
+                material: materials.add(StandardMaterial {
+                    base_color: Color::rgb(0.3, 0.3, 0.3).into(),
+                    perceptual_roughness: 0.3,
+                    metallic: 0.8,
+                    ..default()
+                }),
+                transform: Transform::from_translation(ev.position),
+                ..default()
+            })
+            .insert(CannonBall {})
+            .insert(Collider::ball(PLAYER_SIZE))
+            .insert(ActiveEvents::COLLISION_EVENTS)
+            .insert(RigidBody::Dynamic)
+            .insert(Damping {
+                linear_damping: 0.1,
+                angular_damping: 0.2,
+            })
+            .insert(ColliderMassProperties::Density(1.0))
+            .insert(GravityScale(0.0))
+            .insert(Friction {
+                coefficient: 2.0,
+                combine_rule: CoefficientCombineRule::Max,
+            })
+            .insert(Restitution {
+                coefficient: 0.0,
+                combine_rule: CoefficientCombineRule::Max,
+            })
+            .insert(ExternalForce {
+                force: Vec3::new(0.0, 0.0, 0.0),
+                torque: Vec3::new(0.0, 0.0, 0.0),
+            })
+            .insert(ExternalImpulse {
+                impulse: ev.direction * PLAYER_IMPULSE_MAGNITUDE,
+                torque_impulse: Vec3::new(0.0, 0.0, 0.0),
+            });
+    }
+}
+
+// System to handle collision events
+fn handle_collisions(
+    mut commands: Commands,
+    mut collision_events: EventReader<CollisionEvent>,
+    player_collider_query: Query<Entity, With<PlayerCollider>>,
+    cannon_ball_query: Query<Entity, With<CannonBall>>,
+) {
+    for collsion_event in collision_events.iter() {
+        // Check only when collision has started
+        if let CollisionEvent::Started(collider, other_collider, _) = collsion_event {
+            // If collider has a PlayerCollider component
+            if let Ok(_entity) = player_collider_query.get(*collider) {
+                // TODO: Set game over
+                // commands.entity(*collider).despawn();
+                commands.entity(*other_collider).despawn();
+            } else if let Ok(entity) = cannon_ball_query.get(*collider) {
+                commands.entity(entity).despawn();
+
+                if let Ok(entity) = cannon_ball_query.get(*other_collider) {
+                    commands.entity(entity).despawn();
+                }
             }
         }
     }
@@ -362,83 +506,6 @@ fn gravity(mut query: Query<(&Transform, &mut ExternalForce)>) {
         let grav_force_magnitude = transform.translation.length().powi(2) * GRAVITY_MAGNITUDE;
         force.force = grav_force_magnitude * -transform.translation.normalize();
     }
-}
-
-// System to handle collision events
-fn handle_collisions(
-    mut commands: Commands,
-    mut collision_events: EventReader<CollisionEvent>,
-    player_collider_query: Query<With<PlayerCollider>>,
-    cannon_ball_query: Query<With<CannonBall>>,
-) {
-    for collsion_event in collision_events.iter() {
-        // Check only when collision has started
-        if let CollisionEvent::Started(collider, other_collider, _) = collsion_event {
-            // If collider has a PlayerCollider component
-            if let Ok(_) = player_collider_query.get(*collider) {
-                // TODO: Set game over
-                // commands.entity(*collider).despawn();
-                commands.entity(*other_collider).despawn();
-            } else if let Ok(_) = cannon_ball_query.get(*collider) {
-                commands.entity(*collider).despawn();
-
-                if let Ok(_) = cannon_ball_query.get(*other_collider) {
-                    commands.entity(*other_collider).despawn();
-                }
-            }
-        }
-    }
-}
-
-// Shoot cannon ball helper function
-fn shoot_cannon_ball(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    position: Vec3,
-    direction: Vec3,
-) {
-    commands
-        .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Icosphere {
-                radius: PLAYER_SIZE / 2.0,
-                subdivisions: 16,
-            })),
-            material: materials.add(StandardMaterial {
-                base_color: Color::rgb(0.3, 0.3, 0.3).into(),
-                perceptual_roughness: 0.3,
-                metallic: 0.8,
-                ..default()
-            }),
-            transform: Transform::from_translation(position),
-            ..default()
-        })
-        .insert(CannonBall {})
-        .insert(Collider::ball(PLAYER_SIZE))
-        .insert(ActiveEvents::COLLISION_EVENTS)
-        .insert(RigidBody::Dynamic)
-        .insert(Damping {
-            linear_damping: 0.1,
-            angular_damping: 0.2,
-        })
-        .insert(ColliderMassProperties::Density(1.0))
-        .insert(GravityScale(0.0))
-        .insert(Friction {
-            coefficient: 2.0,
-            combine_rule: CoefficientCombineRule::Max,
-        })
-        .insert(Restitution {
-            coefficient: 0.0,
-            combine_rule: CoefficientCombineRule::Max,
-        })
-        .insert(ExternalForce {
-            force: Vec3::new(0.0, 0.0, 0.0),
-            torque: Vec3::new(0.0, 0.0, 0.0),
-        })
-        .insert(ExternalImpulse {
-            impulse: direction * PLAYER_IMPULSE_MAGNITUDE,
-            torque_impulse: Vec3::new(0.0, 0.0, 0.0),
-        });
 }
 
 trait TransformExt {
